d7c8419d658850c89f16009aa3d59e84c28c2b89
Joujou58007 <leducjulien169@gmail.com>
2026-02-02 16:13:25 -0500

    inital implementation of the water mission UI. Style yet to do

---
+++ workspaces/ground_station_ws/src/water_web_server_node/include/water_web_server_node/water_web_server_node.hpp
@@ -0,0 +1,91 @@
+#ifndef WATER_WEB_SERVER_NODE_HPP_
+#define WATER_WEB_SERVER_NODE_HPP_
+
+#include <rclcpp/rclcpp.hpp>
+#include <ament_index_cpp/get_package_share_directory.hpp>
+#include "std_msgs/msg/bool.hpp"
+#include <thread>
+#include <fstream>
+#include <string>
+#include <boost/asio/io_context.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/beast/core/string.hpp>
+#include <boost/beast/http/message.hpp>
+#include <boost/beast/http/string_body.hpp>
+#include <boost/beast/http/file_body.hpp>
+#include <boost/beast/websocket.hpp>
+#include <nlohmann/json.hpp>
+#include "custom_interfaces/msg/ui_message.hpp"
+#include <memory>
+
+const std::string WEB_COMPONENT_FOLDER = "/web_components/";
+const int SERVER_PORT = 8080;
+
+// API routes
+const std::string API_MISSION_GO = "/api/mission/go";
+const std::string API_START_LAP = "/api/mission/lap/start";
+const std::string API_FINISH_LAP = "/api/mission/lap/finish";
+const std::string API_MOVE_TO_SCENE = "/api/mission/move_to_scene";
+const std::string API_ABORT_ALL = "/api/mission/abort_all";
+
+namespace beast = boost::beast;
+namespace http = beast::http;
+namespace websocket = beast::websocket;
+namespace net = boost::asio;
+using tcp = boost::asio::ip::tcp;
+using UiMessage = custom_interfaces::msg::UiMessage;
+
+class WaterWebServerNode : public rclcpp::Node
+{
+public:
+    WaterWebServerNode();
+    ~WaterWebServerNode();
+
+    void run_server();
+
+private:
+    void initialize_publisher();
+    void initialize_subscriber();
+
+    // Server Configuration
+    boost::beast::string_view mime_type(boost::beast::string_view path);
+
+    std::string path_cat(boost::beast::string_view base, boost::beast::string_view path);
+
+    void do_session(boost::asio::ip::tcp::socket socket, std::string const &doc_root);
+
+    // Request Handeling
+    http::response<http::string_body> handle_request(std::string const &doc_root, http::request<http::string_body> &&req);
+
+    std::optional<http::response<http::string_body>> try_handle_api(beast::string_view target, const http::request<http::string_body> &req);
+
+    std::optional<http::response<http::string_body>> try_serve_static_file(std::string const &doc_root, beast::string_view target, const http::request<http::string_body> &req);
+
+    // Socket functions
+    void broadcast_status();
+    void broadcast_message(const UiMessage msg);
+    // Variables
+    std::set<websocket::stream<tcp::socket> *> ws_sessions_;
+    std::mutex ws_mutex_;
+    std::atomic<bool> running_{true};
+
+    std::string current_status_ = "IDLE";
+
+    std::string package_share_dir_;
+    std::thread server_thread_;
+
+	rclcpp::Subscription<UiMessage>::SharedPtr message_to_ui_subsciber_;
+
+    rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr mission_go_publisher_;
+    rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr start_lap_publisher_;
+    rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr finish_lap_publisher_;
+
+    rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr move_to_scene_publisher_;
+
+    rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr abort_all_mission_publisher_;
+
+    boost::asio::io_context ioc_;
+    boost::asio::ip::tcp::acceptor acceptor_;
+};
+
+#endif // WATER_WEB_SERVER_NODE_HPP_
---
+++ workspaces/ground_station_ws/src/water_web_server_node/web_components/js/script.js
@@ -0,0 +1,80 @@
+// const API_MISSION_GO = "/api/mission/go";
+// const API_START_LAP = "/api/mission/lap/start";
+// const API_FINISH_LAP = "/api/mission/lap/finish";
+// const API_MOVE_TO_SCENE = "/api/mission/move_to_scene";
+// const API_ABORT_ALL = "/api/mission/abort_all";
+
+// const statusElement = document.getElementById('status');
+// const logElement = document.getElementById('log');
+
+// function appendToLog(message, type = 'info') {
+//     const p = document.createElement('p');
+//     p.textContent = `${new Date().toLocaleString()} - ${message}`;
+//     p.classList.add(type);
+//     logElement.appendChild(p);
+//     logElement.scrollTop = logElement.scrollHeight;
+// }
+
+async function sendCommand(endpoint) {
+    statusElement.textContent = 'Status: Sending command...';
+    try {
+        const response = await fetch(endpoint, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        });
+        if (response.ok) {
+            const data = await response.json();
+            const successMessage = `Success: ${JSON.stringify(data)}`;
+            statusElement.textContent = `Status: ${successMessage}`;
+            appendToLog(`Command to ${endpoint}: ${successMessage}`, 'success');
+        } else {
+            const errorMessage = `Error: ${response.statusText}`;
+            statusElement.textContent = `Status: ${errorMessage}`;
+            appendToLog(`Command to ${endpoint}: ${errorMessage}`, 'error');       
+        }
+    } catch (error) {
+        const errorMessage = `Error: ${error.message}`;
+        statusElement.textContent = `Status: ${errorMessage}`;
+        appendToLog(`Command to ${endpoint}: ${errorMessage}`, 'error');    
+    }
+}
+
+function initalise_buttons() {
+    // document.getElementById('mission-go').addEventListener('click', () => sendCommand(API_MISSION_GO));
+    // document.getElementById('start-lap').addEventListener('click', () => sendCommand(API_START_LAP));
+    // document.getElementById('finish-lap').addEventListener('click', () => sendCommand(API_FINISH_LAP));
+    // document.getElementById('move-to-scene').addEventListener('click', () => sendCommand(API_MOVE_TO_SCENE));
+    // document.getElementById('abort-all').addEventListener('click', () => sendCommand(API_ABORT_ALL));
+
+}
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     initalise_buttons();
+
+//     const ws = new WebSocket("ws://" + window.location.host + "/ws/status");
+
+//     ws.onopen = function() {
+//         console.log("WebSocket connected!");
+//     };
+
+//     ws.onmessage = function(event) {
+//         try {
+//             const data = JSON.parse(event.data);
+//             console.log("Status update:", data);
+//             appendToLog(`Received Message: ${data.message}`, data.is_success ? 'info':'error')
+            
+//         } catch (e) {
+//             console.error("Invalid JSON:", event.data);
+//         }
+//     };
+
+//     ws.onclose = function() {
+//         console.log("WebSocket disconnected");
+//     };
+
+//     ws.onerror = function(error) {
+//         console.error("WebSocket error:", error);
+//     };
+// })
