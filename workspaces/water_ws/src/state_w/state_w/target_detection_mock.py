04cc630a954a0fe8478eff1c29919ca912769670
Joujou58007 <leducjulien169@gmail.com>
2026-04-09 12:38:34 -0400

    change the mock target detection

--- workspaces/water_ws/src/state_w/state_w/target_detection_mock.py
+++ workspaces/water_ws/src/state_w/state_w/target_detection_mock.py
@@ -4,39 +4,53 @@ import rclpy
 from rclpy.node import Node
 from rclpy.qos import QoSProfile, QoSReliabilityPolicy, QoSHistoryPolicy
 
-from custom_interfaces.srv import LocateTarget
-from std_msgs.msg import Header
 from geometry_msgs.msg import PoseStamped
+from std_msgs.msg import Empty
+from zed_msgs.msg import ObjectsStamped, Object
+import time
 
 
 class TargetDetectionMock(Node):
+    @staticmethod
+    def _create_qos_profile(reliability_policy):
+        return QoSProfile(
+            reliability=reliability_policy,
+            history=QoSHistoryPolicy.KEEP_LAST,
+            depth=10
+        )
+    
     def __init__(self):
         super().__init__('target_detection_mock')
-        self.target_pose_srv = self.create_service(
-            LocateTarget,
-            '/aeac/auto_approach/locate_target',
-            self.locate_target_callback
+        qos_reliable = self._create_qos_profile(QoSReliabilityPolicy.RELIABLE)
+
+        self.target_pose_pub = self.create_publisher(
+            ObjectsStamped,
+            '/aeac/internal/auto_approach/target_detected',
+            qos_reliable
+        )
+        
+        self.trigger_target_detection_sub = self.create_subscription(
+            Empty,
+            '/aeac/internal/auto_approach/detect_target',
+            self.locate_target_callback,
+            qos_reliable
         )
         
         self.circle_pos = [5.4, 6.01, 1.0]
+                
+    def locate_target_callback(self, _: Empty):
+        obects_stamped = ObjectsStamped()            
+        object = Object()
         
-    def locate_target_callback(self, request, response):
-        position = PoseStamped()        
+        time.sleep(2)
+        
+        object.label = "Red"
+        object.confidence = float(0.8)
+        object.position = [float(self.circle_pos[0]), float(self.circle_pos[1]), float(self.circle_pos[2])]
+        
+        obects_stamped.objects.append(object)
 
-        if not request.activate:
-            self.get_logger().info("Locate target called but activate=False → ignoring")
-            return position
-        
-        position.header.stamp = self.get_clock().now().to_msg()
-        position.header.frame_id = 'map'
-        
-        position.pose.position.x = self.circle_pos[0]
-        position.pose.position.y = self.circle_pos[1]
-        position.pose.position.z = self.circle_pos[2]
-
-        position.pose.orientation.w = 0.0
-        response.target = position
-        return response
+        self.target_pose_pub.publish(obects_stamped)
     
     
 def main(args=None):
